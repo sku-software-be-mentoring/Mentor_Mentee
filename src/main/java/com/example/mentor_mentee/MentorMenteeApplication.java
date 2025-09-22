ba6ca774bc93d1051cf3e81bba81bd34d8ec88b3
nahjjun <nahjjun1125@naver.com>
2025-09-22 17:58:24 +0900

    init

---
+++ src/main/java/com/example/mentor_mentee/MentorMenteeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentor_mentee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorMenteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorMenteeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mentor_mentee/MentorMenteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentor_mentee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMenteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
