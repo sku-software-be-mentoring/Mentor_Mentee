7a28b7cd54ecc725486b83a7af31cdcd951173c8
nahjjun <nahjjun1125@naver.com>
2025-11-11 18:46:43 +0900

    .

--- src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
+++ src/main/java/com/example/mentor_mentee/domain/post/entity/Post.java
@@ -1,15 +1,14 @@
 package com.example.mentor_mentee.domain.post.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.example.mentor_mentee.domain.comment.entity.Comment;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,6 +32,10 @@ public class Post {
     @Column(nullable = false)
     @Builder.Default
     private Long views = 0L;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE,  orphanRemoval = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
 
     public void update(String title, String content) {
         this.title = title;
