279423682790be112867f4e5ebe3f8af281dfdd6
Ksieon <keumsiun0503@gmail.com>
2025-11-24 20:26:53 +0900

    :recycle: Refactor: add ResponseEntity, BaseResponse

--- src/main/java/com/example/mentor_mentee/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/mentor_mentee/domain/comment/controller/CommentController.java
@@ -3,12 +3,12 @@ package com.example.mentor_mentee.domain.comment.controller;
 import com.example.mentor_mentee.domain.comment.dto.request.CommentRequestDto;
 import com.example.mentor_mentee.domain.comment.dto.response.CommentResponseDto;
 import com.example.mentor_mentee.domain.comment.service.CommentService;
+import com.example.mentor_mentee.global.base.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +21,14 @@ public class CommentController {
     private final CommentService commentService ;
 
     @PostMapping("/{post-id}")
-    public CommentResponseDto createComment(@PathVariable(value = "post-id") Long postId, @RequestBody CommentRequestDto commentRequestDto) {
+    public ResponseEntity<BaseResponse<CommentResponseDto>> createComment(@PathVariable(value = "post-id") Long postId, @RequestBody CommentRequestDto commentRequestDto) {
         CommentResponseDto responseDto = commentService.createComment(postId, commentRequestDto);
-        return responseDto;
+        return ResponseEntity.status(201).body(BaseResponse.success(201, "댓글이 성공적으로 생성되었습니다.", responseDto));
     }
 
     @DeleteMapping("/{post-id}/{comment-id}")
-    public String deleteComment(@PathVariable(value = "post-id") Long postId, @PathVariable(value = "comment-id") Long commentId){
-        return commentService.deleteComment(postId, commentId);
+    public ResponseEntity<BaseResponse<Void>> deleteComment(@PathVariable(value = "post-id") Long postId, @PathVariable(value = "comment-id") Long commentId){
+        commentService.deleteComment(postId, commentId);
+        return ResponseEntity.status(200).body(BaseResponse.success(200, "댓글이 성공적으로 삭제되었습니다.", null));
     }
 }
--- src/main/java/com/example/mentor_mentee/domain/post/controller/PostController.java
+++ src/main/java/com/example/mentor_mentee/domain/post/controller/PostController.java
@@ -5,8 +5,11 @@ import com.example.mentor_mentee.domain.post.dto.request.UpdatePostRequestDto;
 import com.example.mentor_mentee.domain.post.dto.response.PostListResponseDto;
 import com.example.mentor_mentee.domain.post.dto.response.PostResponseDto;
 import com.example.mentor_mentee.domain.post.service.PostService;
+import com.example.mentor_mentee.global.base.BaseResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -16,33 +19,33 @@ public class PostController {
     private final PostService postService ;
 
     @PostMapping
-    public PostResponseDto createPost(@RequestBody CreatePostRequestDto createPostRequestDto) {
+    public ResponseEntity<BaseResponse<PostResponseDto>> createPost(@RequestBody CreatePostRequestDto createPostRequestDto) {
         PostResponseDto responseDto = postService.createPost(createPostRequestDto);
-        return responseDto;
+        return ResponseEntity.status(201).body(BaseResponse.success(201, "게시글이 성공적으로 생성되었습니다.", responseDto));
     }
 
     @GetMapping
-    public List<PostListResponseDto> getAllPosts() {
+    public ResponseEntity<BaseResponse<List<PostListResponseDto>>> getAllPosts() {
         List<PostListResponseDto> responseDtos = postService.readPostList();
-        return responseDtos;
+        return ResponseEntity.status(200).body(BaseResponse.success(responseDtos));
     }
 
     @GetMapping("/{post-id}")
-    public PostResponseDto getPostById(@PathVariable(value = "post-id") Long id) {
+    public ResponseEntity<BaseResponse<PostResponseDto>> getPostById(@PathVariable(value = "post-id") Long id) {
         PostResponseDto responseDto = postService.readPost(id);
-        return responseDto;
+        return ResponseEntity.status(200).body(BaseResponse.success(responseDto));
     }
 
     @PutMapping("/{post-id}")
-    public PostResponseDto updatePost(@RequestBody UpdatePostRequestDto updatePostRequestDto, @PathVariable(value = "post-id") Long id) {
+    public ResponseEntity<BaseResponse<PostResponseDto>> updatePost(@RequestBody UpdatePostRequestDto updatePostRequestDto, @PathVariable(value = "post-id") Long id) {
         PostResponseDto responseDto = postService.updatePost(updatePostRequestDto, id);
-        return responseDto;
+        return ResponseEntity.status(200).body(BaseResponse.success(responseDto));
     }
 
     @DeleteMapping("/{post-id}")
-    public String deletePost(@PathVariable(value = "post-id") Long id){
-        String response = postService.deletePost(id);
-        return response;
+    public ResponseEntity<BaseResponse<Void>> deletePost(@PathVariable(value = "post-id") Long id){
+        postService.deletePost(id);
+        return ResponseEntity.status(200).body(BaseResponse.success(200, "게시글 성공적으로 삭제되었습니다.", null));
     }
 
 }
